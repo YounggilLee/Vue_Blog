5d94b73417a6ef8434cf462c9f7dc5424515682c
GiL <iamleeyounggil@gmail.com>
2018-01-19 15:07:13 -0500

    Install Vuex and vue-router and blog pages

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import addBlog from './components/addBlog.vue';
+import showBlogs from './components/showBlogs.vue';
+import singleBlog from './components/singleBlog.vue';
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', component: showBlogs},
+    { path: '/add', component: addBlog},
+    { path: '/blog/:id', component: singleBlog}
+
+]
+
+export default new VueRouter({mode: 'history', routes})
